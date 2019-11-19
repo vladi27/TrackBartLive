7e1bde6150f5f9499ace5116bda500c066680a5d
vladi27 <vbulvakhter@gmail.com>
2019-11-19 14:38:59 -0800

    fix bart api calls

--- src/util/station_api_util.js
+++ src/util/station_api_util.js
@@ -32,7 +32,7 @@ const stationsSouthBound = [
 
 export const fetchCurrentEtas = () => {
   return axios.get(
-    "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&key=MW9S-E7SL-26DU-VV8V&json=y"
+    "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&key=MW9S-E7SL-26DU-VV8V&json=y"
   );
 };
 
@@ -44,18 +44,18 @@ export const getStations = () => {
 export const getStation = abbr => {
   console.log(abbr);
   return axios.get(
-    `http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=${abbr}&key=MW9S-E7SL-26DU-VV8V&json=y`
+    `https://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=${abbr}&key=MW9S-E7SL-26DU-VV8V&json=y`
   );
 };
 export const getRoutes = () => {
   return axios.get(
-    "http://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V&date=now&json=y"
+    "https://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V&date=now&json=y"
   );
 };
 
 export const getSchedules = id => {
   return axios.get(
-    `http://api.bart.gov/api/sched.aspx?cmd=routesched&route=${id}&key=MW9S-E7SL-26DU-VV8V&date=now&json=y`
+    `https://api.bart.gov/api/sched.aspx?cmd=routesched&route=${id}&key=MW9S-E7SL-26DU-VV8V&date=now&json=y`
   );
 };
 export const getStationDepartures = abbr => {
@@ -66,6 +66,6 @@ export const getStationDepartures = abbr => {
 
 export const getRouteStations = id => {
   return axios.get(
-    `http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=${id}&key=MW9S-E7SL-26DU-VV8V&json=y`
+    `https://api.bart.gov/api/route.aspx?cmd=routeinfo&route=${id}&key=MW9S-E7SL-26DU-VV8V&json=y`
   );
 };
