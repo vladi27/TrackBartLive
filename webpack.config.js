d1154245388cf41837081a839bb08908a5e4c7f8
vladi27 <vbulvakhter@gmail.com>
2021-06-09 19:53:49 -0700

    Edit modal height

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,55 @@
 const webpack = require("webpack");
 var path = require("path");
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "dist"),
-    // publicPath: "/dist",
-    // publicPath: "/",
-    filename: "bundle.js",
-    publicPath: argv.mode === "production" ? "/my-static-website" : "/",
-  },
-  //devServer: { contentBase: "dist" },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true,
+module.exports = (env, argv) => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "dist"),
+      // publicPath: "/dist",
+      // publicPath: "/",
+      filename: "bundle.js",
+      publicPath: argv.mode === "production" ? "/my-static-website" : "/",
+    },
+    //devServer: { contentBase: "dist" },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
             },
-          },
-        ],
-      },
-    ],
-  },
-  //devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", "*"],
-  },
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[name]_[local]_[hash:base64]",
+                sourceMap: true,
+                minimize: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    //devtool: "source-map",
+    resolve: {
+      extensions: [".js", ".jsx", "*"],
+    },
+  };
 };
