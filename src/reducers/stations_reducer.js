32083cd1c9ffdc60e50c2695bdba9d112d6753c5
vladi27 <vbulvakhter@gmail.com>
2021-06-09 18:01:19 -0700

    Remove comments

--- src/reducers/stations_reducer.js
+++ src/reducers/stations_reducer.js
@@ -8,7 +8,7 @@ import merge from "lodash/merge";
 
 const StationsReducer = (state = {}, action) => {
   Object.freeze(state);
-  // let newState = Object.assign({}, state)
+
   switch (action.type) {
     case RECEIVE_STATIONS:
       let newObj = {};
