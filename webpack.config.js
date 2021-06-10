6c65692aa0edef54e3be5f3ff95ada365c6911b1
vladi27 <vbulvakhter@gmail.com>
2021-06-09 19:24:21 -0700

    Fix image path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     // publicPath: "/dist",
-    publicPath: "/",
+    // publicPath: "/",
     filename: "bundle.js",
   },
   //devServer: { contentBase: "dist" },
